9671e330ef74e9a738d5094befe5bb2bee1921f5
RafaelAsafe <asafe.ferr@gmail.com>
2022-05-21 02:12:51 -0300

    "arquitetura_flask"

---
+++ Vitreus/blueprints/webiu/views.py
@@ -0,0 +1,21 @@
+from Flask import abort, render_template
+
+def index():
+    
+    return render_template("index.html")
+
+def sobre():
+    return render_template('sobre.html')
+
+def error():
+    MsgError = f'página "{nome}" não existe'
+    return render_template("error404.html", MsgError2=MsgError)
+
+def post_arquivo():
+    arquivo = request.files.get("meuArquivo")
+    print(arquivo)
+    nome_do_arquivo = arquivo.filename
+    arquivo.save(os.path.join(DIRETORIO, nome_do_arquivo))
+    return 'Arquivo Recebido com sucesso', 201
+
+
