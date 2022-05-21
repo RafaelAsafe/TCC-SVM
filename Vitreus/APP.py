6acd08923bee43cfcf9c501199a070017e97c80d
RafaelAsafe <asafe.ferr@gmail.com>
2022-05-21 02:45:55 -0300

    arquitetura_flask

--- Vitreus/APP.py
+++ Vitreus/APP.py
@@ -1,37 +1,14 @@
 from flask import Flask
 from vitreus.ext import configuration
 
-app = Flask(__name__)
-Bootstrap(app)
+
+def minimal_app():
+    app = Flask(__name__)
+    configurations.init_app(app)
+    return app
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return render_template("index.html",**locals())# rederizar paginas html -**locals() passa todas as variaveis do app pro template má pratica usar somente no dev!!!!!!
-
-
-@app.route('/sobre')
-def sobre():
-    return render_template('sobre.html')
-
-
-@app.route('/<string:nome>')
-def error(nome):
-    MsgError = f'página "{nome}" não existe'
-    return render_template("error404.html", MsgError2=MsgError)
-
-
-@app.route("/arquivos", methods=["POST"])
-def post_arquivo():
-    arquivo = request.files.get("meuArquivo")
-
-    print(arquivo)
-    nome_do_arquivo = arquivo.filename
-    arquivo.save(os.path.join(DIRETORIO, nome_do_arquivo))
-
-    return 'Arquivo Recebido com sucesso', 201
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+def create_app():
+    app = minimal_app()
+    configurations.load_extensions(app)
+    return app
