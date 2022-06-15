9e8b549bd055008966e39ab66a219f1fe118b8c6
Rafael_Asafe <asafe.ferr@gmail.com>
2022-06-15 00:45:48 -0300

    add crud patients_api

--- Vitreus/blueprints/patient_api/resources.py
+++ Vitreus/blueprints/patient_api/resources.py
@@ -1,8 +1,38 @@
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from flask_restful import Resource
-from vitreus.ext.database import create_patient, create_exam
-
 from vitreus.models import Patient, Exam
+from flask_sqlalchemy import SQLAlchemy
+from vitreus.ext.database import db
+
+#adicionar autenticação 
+#adicioanar reposta quando não encontrar paciente ou exame
+#tratamento de erro 
+#tentar fazer uma função de update 
+
+def create_patient(name,description, diagnosis):
+    """Registra um novo paciente caso nao esteja cadastrado"""
+    #aprimorar o requisito de usuário já cadastrado para cpf 
+    if Patient.query.filter_by(name=name).first():
+        raise RuntimeError(f'{name} ja esta cadastrado')
+    patient_obj = Patient(name=name,description=description,diagnosis=diagnosis)
+    db.session.add(patient_obj)
+    db.session.commit()
+    return patient_obj
+
+def create_exam(data,storage_ref,patient_id):
+    """Registra um novo exame"""
+    #aprimorar para evitar exames repetidos 
+    exam = Exam(data=data,storage_ref=storage_ref,patient_id=patient_id)
+    db.session.add(exam)
+    db.session.commit()
+    return exam
+
+#def update_patient(id,name,description, diagnosis):
+    
+    db.session.add(patient_obj)
+    db.session.commit()
+
+DIRETORIO = "ASDASDASD"
 
 class PatientResourse(Resource):
     def get(self):
@@ -12,7 +42,16 @@ class PatientResourse(Resource):
         )
 
     def post (self):
-       create_patient()
+        body = request.get_json()
+
+        try:
+            patient_data = {"name":body['name'], "description":body['description'], "diagnosis":'unknown'} 
+            patient = create_patient(patient_data["name"],patient_data["description"],patient_data["diagnosis"])
+            return jsonify(patient.to_dict())
+
+        except Exception as e:
+            print(e)
+            return 400, "erro ao cadastrar"
 
 class PatientNumberResourse(Resource):
     def get(self, patient_id):
@@ -20,6 +59,39 @@ class PatientNumberResourse(Resource):
             404 
         )
         return jsonify(patient.to_dict())
+   
+    def put(self, patient_id):
+        patient = Patient.query.filter_by(id=patient_id).first()
+        body = request.get_json()
+        
+        try:
+            if('name' in body):
+                patient.name = body['name']
+            if('description' in body):
+                patient.description = body['description']
+            if('diagnosis' in body):
+                patient.diagnosis = body['diagnosis']
+            
+            db.session.add(patient)
+            db.session.commit()
+            
+            return jsonify(patient.to_dict())
+
+        except Exception as e:
+            print('Erro', e)
+            return 400, "erro ao atualizar paciente"
+
+    def delete(self,patient_id):
+        patient = Patient.query.filter_by(id=patient_id).first()
+
+        try:
+            db.session.delete(patient)
+            db.session.commit()
+            return jsonify("deletado com sucesso",patient.to_dict())
+
+        except Exception as e:
+            print('Erro', e)
+            return 400, "Erro ao deletar"
 
 class ExamResourse(Resource):
     def get (self,patient_id):
@@ -28,5 +100,15 @@ class ExamResourse(Resource):
             {"exams":[exam.to_dict() for exam in exams]
             }
         )
-    def post (self):
-      create_exam()
+    def post (self,patient_id):
+        body = request.get_json()
+        storage_path= "criar"
+
+        try:
+            exam_obj = {"data":body[date], "storage_ref": storage_path , "patient_id":patient_id} 
+            exam = create_exam(patient_data["name"],patient_data["description"],patient_data["diagnosis"])
+            return jsonify(exam.to_dict())
+
+        except Exception as e:
+            print(e)
+            return 400, "erro ao cadastrar"
