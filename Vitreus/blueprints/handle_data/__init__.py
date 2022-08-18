3c53eef24cd2fce409c8870432d1c90f974665b1
RafaelAsafe <asafe.ferr@gmail.com>
2022-08-18 00:02:14 -0300

    atualizações agosto

--- Vitreus/blueprints/handle_data/__init__.py
+++ Vitreus/blueprints/handle_data/__init__.py
@@ -1,11 +1,13 @@
 from flask import Blueprint
 from flask_restful import Api
 
-#from .tratar_data import 
+# from .tratar_data import
 
 
-bp = Blueprint("restapi", __name__, url_prefix="/api/v1")
+bp = Blueprint("handle_data", __name__, url_prefix="/api/v1")
 api = Api(bp)
+
+
 def init_app(app):
     #api.add_resource(ProductResource, "/product/")
     #api.add_resource(ProductItemResource, "/product/<product_id>")
--- Vitreus/blueprints/svm/svm_model.py
+++ Vitreus/blueprints/svm/svm_model.py
@@ -1,20 +1,19 @@
-#converter em rest api e finalizar 
+# converter em rest api e finalizar
 from sklearn import svm
 from sklearn import metrics
-0
-#loading the dataset
+# loading the dataset
 data = datasets.load_digits()
-#create a classifier
+# create a classifier
 cls = svm.SVC(kernel="linear")
-#train the model
-cls.fit(X_train,y_train)
-#predict the response
+# train the model
+cls.fit(X_train, y_train)
+# predict the response
 pred = cls.predict(X_test)
 
-#accuracy
-print("acuracy:", metrics.accuracy_score(y_test,y_pred=pred))
-#precision score
-print("precision:", metrics.precision_score(y_test,y_pred=pred))
-#recall score
-print("recall" , metrics.recall_score(y_test,y_pred=pred))
+# accuracy
+print("acuracy:", metrics.accuracy_score(y_test, y_pred=pred))
+# precision score
+print("precision:", metrics.precision_score(y_test, y_pred=pred))
+# recall score
+print("recall", metrics.recall_score(y_test, y_pred=pred))
 print(metrics.classification_report(y_test, y_pred=pred))
